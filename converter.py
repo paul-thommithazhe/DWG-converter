6b11616fb469742967ef60b6c5bc8b0cd9bc79f0
Thushad T <65484981+Thushad-T@users.noreply.github.com>
2021-02-18 16:37:41 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -24,3 +24,39 @@ Label_3.place(x=80, y=80)
 Button(root, text="DWG_to_JPG", width=20, height=2, bg="brown", fg="white", command=dwg_to_jpg).place(x=120, y=120)
 
 root.mainloop()
+
+
+
+
+
+
+"""
+#for converting jpg to png
+
+from tkinter import *
+from tkinter import filedialog as fd
+import os
+from PIL import ImageTk, Image
+root = Tk()
+root.geometry("400x400")
+root.title("Image_Conversion_App")
+
+
+def jpg_to_png():
+    filename = fd.askopenfilename()
+    if filename.endswith(".jpg"):
+        Image.open(filename).save("edited.png")
+        Label_1 = Label(root, text="Done", width=20, fg="green", font=("bold", 15))
+        Label_1.place(x=80, y=280)
+    else:
+        Label_2 = Label(root, text="Error!", width=20, fg="red", font=("bold", 15))
+        Label_2.place(x=80, y=280)
+
+
+Label_3 = Label(root, text="Choose File", width=20, font=("bold", 15))
+Label_3.place(x=80, y=80)
+
+Button(root, text="JPG_to_PNG", width=20, height=2, bg="brown", fg="white", command=jog_to_png).place(x=120, y=120)
+
+root.mainloop()
+"""
